3d1b5222a40361f5747aa223b0c328827532e8c6
surya-situ <suryasocial55@gmail.com>
2024-04-08 14:43:58 +0530

    Home page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+
+const Home = () => {
+    return (
+      <div className="bg-[#18191b] min-h-screen w-full">
+          <div className="bg-[#252627] shadow-md">
+              <div className="w-[93%] m-auto py-3">
+                  <div className="flex items-center justify-between">
+                      <div className="w-[120px] h-[48px] text-white">
+                          maya-palette
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+    )
+  }
+  
+  export default Home
