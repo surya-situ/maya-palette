3955d51db579026262a2124dea485b42e51f88e5
surya-situ <suryasocial55@gmail.com>
2024-04-08 14:43:09 +0530

    Implemented react-router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
 import App from './App.jsx'
 import './index.css'
 
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={appRouter} />
 )
