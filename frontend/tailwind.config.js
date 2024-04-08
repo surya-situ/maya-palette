7e04c5fa720d070e63fcdeaabe356beeb6f11b16
surya-situ <suryasocial55@gmail.com>
2024-04-08 14:44:16 +0530

    tailwind config

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./index.html",
+  "./src/**/*.{html,js,jsx,tsx}"
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
