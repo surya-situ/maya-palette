b9bc03b4f59cdf9bf632f662fa79879e102e722e
surya-situ <suryasocial55@gmail.com>
2024-04-12 19:58:19 +0530

    changed the react router components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,30 @@
 import ReactDOM from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from "react-router-dom"
-import App from './App.jsx'
+
 import './index.css'
+// import App from './App.jsx'
+import Layout from './pages/Layout.jsx'
+import Home from './components/Home.jsx'
+import Templates from './components/Templates.jsx'
+import Projects from './components/Projects.jsx'
 
 const appRouter = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <Layout/>,
     children: [
-      
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/templates',
+        element: <Templates />
+      },
+      {
+        path: '/projects',
+        element: <Projects />
+      },
     ]
   }
 ])
